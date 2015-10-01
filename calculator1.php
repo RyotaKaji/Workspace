f0af8efd5476fd1636e1630c324f1cad93b37a6c
Ryota Kaji <ryota.lives.here@gmail.com>
2015-10-01 17:03:52 +0900

    コメントアウトされた箇所の追加

--- calculator1.php
+++ calculator1.php
@@ -1,30 +1,49 @@
 <?php
-require_once 'Encode.php'; 
 
  $post = $_POST;
 
- $number1 = $post['number1'];
- $number2 = $post['number2'];
- $kigou = "";
- $result = "";
- 
- if (isset($post['kigou'])){
-    $kigou = $post['kigou'];
+//変数が定義されているか
+//変数がnull/blank(empty関数)でないか→初期resultも消える
+ if (!isset($post['kigou'])) {
+    if (empty($post['kigou'])) {
+        $kigou = "";
+    }
 }
 
- switch ($kigou) {
-    case '+' :
-        $result = $number1 + $number2;
-        break;
-    case '-' :
-        $result = $number1 - $number2;
-        break;
-    case '×' :
-        $result = $number1 * $number2;
-        break;
-    case '÷' :
-        $result = $number1 / $number2;
-        break;
+//それぞれの入力値が数字かどうかチェックする
+
+ if (is_numeric($post['number1'])) {
+    $number1 = $post['number1'];
+}
+
+ if (is_numeric($post['number2'])) {
+    $number2 = $post['number2'];
+}
+
+ 
+//記号チェック(nullじゃないかの確認も)
+
+ if (isset($post['kigou'])){
+    if ($post['kigou'] == 0 or $post['kigou'] == 1 or $post['kigou'] == 2 or $post['kigou'] == 3) { 
+       $kigou = $post['kigou'];
+}
+}
+
+
+//計算する＆ゼロ除算を防ぐ
+
+if ($kigou == '0') {
+    $result = $number1 + $number2;
+} else if ($kigou == '1') {
+    $result = $number1 - $number2;
+} else if ($kigou == '2') {
+    $result = $number1 * $number2;
+} else if ($kigou == '3' && $number2 == 0) {
+    $result = 'error';
+} else if ($kigou == '3' && $number2 != 0) {
+    $result = $number1 / $number2;
+} else {
+    $result = "";
 }
 
 ?>
@@ -36,20 +55,20 @@ require_once 'Encode.php';
 
 <form method = "POST" action = "calculator1.php">
 
-<input type = "text" name = "number1" size = "10" value ="<?php print $number1; ?>" />
+<input type = "number" name = "number1" size = "10" value = "<?php print $number1; ?>" />
 
 <select name = "kigou">
-<option value ="+" <?php if ($kigou == '+'){print 'selected';} ?>> + </option>
-<option value ="-" <?php if ($kigou == '-'){print 'selected';} ?>> − </option>
-<option value ="×"<?php if ($kigou == '×'){print 'selected';} ?>> × </option>
-<option value ="÷"<?php if ($kigou == '÷'){print 'selected';} ?>> ÷ </option>
+<option value ="0" <?php if ($kigou == '0'){print 'selected';} ?>> + </option>
+<option value ="1" <?php if ($kigou == '1'){print 'selected';} ?>> − </option>
+<option value ="2"<?php if ($kigou == '2'){print 'selected';} ?>> × </option>
+<option value ="3"<?php if ($kigou == '3'){print 'selected';} ?>> ÷ </option>
 </select>
 
-<input type = "text" name = "number2" size = "10" value ="<?php print $number2; ?>"/>
+<input type = "number" name = "number2" size = "10" value = "<?php print $number2; ?>" />
 
 <input type = "submit" value = "=" />
 
-<span><?php print e($result); ?></span>
+<span><?php print $result; ?></span>
 
 
 </form>
